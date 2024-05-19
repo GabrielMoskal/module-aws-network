1a9012567a216ecff12d5777f1ae889b376db835
Gabriel Moskal <gab.mos92@gmail.com>
2024-05-19 14:28:21 +0200

    network module created

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = "mock_access_key"
+  secret_key = "mock_secret_key"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 }
 
 locals {
